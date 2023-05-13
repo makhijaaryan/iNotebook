083d6cfcaf44481e4bb191fd6da98a054629fb04
makhijaaryan <115446925+makhijaaryan@users.noreply.github.com>
2023-05-13 23:03:37 +0530

    .

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,11 @@
 const mongoose= require('mongoose');
-const mongoURI = "mongodb+srv://aryanmakhija0407:Jl9lMb8wzcdzc7DD@inotebook.yqlfjvm.mongodb.net/?retryWrites=true&w=majority";
-// const mongoURI = `mongodb+srv://{procee.env.REACT_APP_ID}:{procee.env.REACT_APP_PASSWORD}@inotebook.yqlfjvm.mongodb.net/?retryWrites=true&w=majority`;
 
 
 
 const connectToMongo = async () => {
     try {
         mongoose.set('strictQuery', false)
-        mongoose.connect(mongoURI) 
+        mongoose.connect(process.env.MONGO_URI) 
         console.log('Mongo connected')
     }
     catch(error) {
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express');
 
-
-
+require('dotenv').config()
 connectToMongo();
 
 const app = express()
